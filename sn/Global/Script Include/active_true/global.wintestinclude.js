17deb9e90c2610f8a7338ecc7e8653dba5d2bc4d
System Administrator <admin@example.com>
2019-05-30 19:37:03 +0000

    Changes by 'System Administrator' on 'wintest' -- Build #1

---
+++ sn/Global/Script Include/active_true/global.wintestinclude.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-05-30 19:34:55
+ * Created By  : admin
+ * Updated On  : 2019-05-30 19:34:55
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=c947176013353700aee432228144b0bf
+ */
+var wintestinclude = Class.create();
+wintestinclude.prototype = {
+    initialize: function() {
+    },
+
+    type: 'wintestinclude'
+};
