b694bf94bf50d8702020f1e7a13105bda290631f
System Administrator <admin@example.com>
2019-05-30 22:24:52 +0000

    Changes by 'System Administrator' on 'wintest 4' -- Build #1

---
+++ sn/Global/Script Include/active_true/global.newwintest4.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-05-30 22:24:22
+ * Created By  : admin
+ * Updated On  : 2019-05-30 22:24:22
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=a51ef768db75f700e87dd49fdf9619e6
+ */
+var newwintest4 = Class.create();
+newwintest4.prototype = {
+    initialize: function() {
+    },
+
+    type: 'newwintest4'
+};
