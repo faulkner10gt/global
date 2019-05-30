46b5a01340f24056d260d0090ac4ea286b9377b5
System Administrator <admin@example.com>
2019-05-30 22:11:34 +0000

    Changes by 'System Administrator' on 'wintest 3' -- Build #1

---
+++ sn/Global/Script Include/active_true/global.si55.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : Global
+ * ClassName   : sys_script_include
+ * Created On  : 2019-05-30 22:07:05
+ * Created By  : admin
+ * Updated On  : 2019-05-30 22:07:05
+ * Updated By  : admin
+ * URL         : /sys_script_include.do?sys_id=071a3f64db75f700e87dd49fdf961935
+ */
+var si55 = Class.create();
+si55.prototype = {
+    initialize: function() {
+    },
+
+    type: 'si55'
+};
